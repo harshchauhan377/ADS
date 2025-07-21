8f79efaca689478b3a147793408079ee1480a416
hc <hcrajput321@gmail.com>
2025-07-21 10:03:35 +0530

    half backtracking basic

--- _11_backtracking_L2.java
+++ _11_backtracking_L2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +39,38 @@ public class _11_backtracking_L2{
         back3(candidates , 0 , target2 , curr4 , res4);
         System.out.print("combination 1 : ");
         System.out.println(res4);
+
+        ///////////////////////////////////////////////////////////////////////////////////
+        //  combination 2
         
+        int[]candidate = {10,1,2,7,6,1,5}; int target3 = 8;
+        Arrays.sort(candidate);
+        List<Integer>curr5 = new ArrayList<>();
+        List<List<Integer>> res5 = new ArrayList<List<Integer>>();
+        back4(candidate , 0 , target3 , curr5 , res5);
+        System.out.print("combination 2 : ");
+        System.out.println(res5);
+
+        ///////////////////////////////////////////////////////////////////////////////////
+        //  combination 3
+        int k6 = 3 ; int n6 = 7;
+        List<Integer>curr6 = new ArrayList<>();
+        List<List<Integer>>res6 = new ArrayList<List<Integer>>();
+        back5(1 , k6 , n6 , curr6 , res6);
+        System.out.print("combination 3 : ");
+        System.out.println(res6);
+
+        ///////////////////////////////////////////////////////////////////////////////////
+        // COmbination 4
+        int []nums = {1,2,3} ; int target = 4 ;
+        int dp[] = new int[target+1];
+        Arrays.fill(dp,-1);
+        System.out.print("combination 4 : ");
+        int ans = dpM(nums , target , dp);
+        System.out.println(ans);
+
+
+
     }
     //print all Subsequence of String in lexicographically order
     public static void back(String s , int idx , List<String>res , String curr , int n){
@@ -116,6 +148,82 @@ public class _11_backtracking_L2{
         curr.remove(curr.size()-1);
         back3(arr , idx + 1 , target , curr , res);
 
+    }
+
+    // combination 2    // no duplicate 
+    //Input: candidates = [10,1,2,7,6,1,5], target = 8
+    //Output: [[1,1,6],[1,2,5],[1,7],[2,6]]
+
+    public static void back4(int arr[] , int idx , int target , List<Integer>curr , List<List<Integer>>res ){
+        if(target == 0){
+            res.add(new ArrayList<>(curr));
+            return ;
+        }
+
+        // one way 
+
+        // for(int i = idx ; i< arr.length ; i++){
+        //     if( (i == idx || arr[i-1] != arr[i]) && arr[i] <= target ){
+        //         curr.add(arr[i]);
+        //         back4(arr ,  i + 1 , target - arr[i], curr , res );
+        //         curr.remove(curr.size()-1);
+        //     }
+        // }
+
+        // sec way 
+        for(int i = idx ; i< arr.length ; i++){
+
+            if(i > idx && arr[i] == arr[i-1]) continue ;
+            if(arr[i] > target) break;
+
+            curr.add(arr[i]);
+            back4(arr ,  i + 1 , target - arr[i] , curr , res );
+            curr.remove(curr.size()-1);
+        }
+    }
+
+    // Combination 3
+    //Input: k = 3, n = 7
+    //Output: [[1,2,4]]
+    //Explanation: 1 + 2 + 4 = 7
+
+    public static void back5(int n , int k , int target , List<Integer>curr  , List<List<Integer>>res){
+        if(target < 0 || k < 0) return ;
+
+        if(target == 0 && k == 0){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for(int i = n ; i <= 9 ; i ++){
+            curr.add(i);
+            back5(i+1 , k-1 , target - i , curr , res );
+            curr.remove(curr.size()-1);
+        }
+    }
+
+    // Combination 4
+    // Input: nums = [1,2,3], target = 4
+    // Output: 7
+    // Explanation: The possible combination ways are:
+    // [(1, 1, 1, 1),(1, 1, 2),(1, 2, 1),(1, 3),(2, 1, 1),(2, 2),(3, 1)]
+
+    // public static int count = 0 ;
+    public static int dpM(int arr[] , int target , int []dp){
+        if(target == 0) return 1 ;
+        if(target < 0) return 0 ;
+
+        if(dp[target] != -1){
+            return dp[target];
+        }
+
+        int count = 0 ;
+        for(int i = 0 ; i< arr.length ; i++){
+            count += dpM(arr , target - arr[i] , dp);
+        }
+
+        dp[target] = count ;
+        return dp[target];
     }
 
 }
